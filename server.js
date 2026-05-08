e2fcc87f866c0b654ad4137eb84919c35eec415b
Yanis <yahiaouiyanis15@gmail.com>
2026-05-08 15:28:38 +0200

    fix cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
